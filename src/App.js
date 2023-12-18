1f347aad3256e5c3606c2f77d3f770d0233d542f
VSRudnyk <v.rudnyk@gmail.com>
2023-12-18 17:47:30 +0200

    test3

--- src/App.js
+++ src/App.js
@@ -6,39 +6,30 @@ import { Auth } from './Components/Auth/Auth';
 import { Widget } from './Components/Widget/Widget';
 import { useSelector } from 'react-redux';
 import authSelectors from '../src/redux/authSelectors';
-import { useFetchCurrentBotQuery } from './redux/authAPI';
+import { useFetchCurrentBotQuery, useAuthMutation } from './redux/authAPI';
 import { useSearchParams } from 'react-router-dom';
 import './index.css';
+import { useEffect } from 'react';
 
 function App() {
+  const [logIn, {isSuccess}] = useAuthMutation();
   const [searchParams] = useSearchParams();
   const id = searchParams.get('id');
+ 
   const token = useSelector(authSelectors.getToken);
   useFetchCurrentBotQuery(null, { skip: !token });
+
+  useEffect(() => {
+    const loginBot = async (botId) => {
+      await logIn(botId)
+    };
+    loginBot({_id: id})
+  }, []);
+
   return (
-    <Routes>
-      <Route
-      path='/'
-      element={<Layout />}
-      >
-        <Route
-          path='/auth'
-          element={
-            <PublicRoute restricted>
-              <Auth id={id}/>
-            </PublicRoute>
-          }
-        />
-        <Route
-          path='/widget'
-          element={
-            <PrivateRoute>
-              <Widget />
-            </PrivateRoute>
-          }
-        />
-        </Route>
-    </Routes>
+    <>
+    {isSuccess && <Widget/>}
+    </>
   );
 }
 
